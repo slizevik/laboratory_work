cfd5d3f1e7a926175325cf3654b30a18d18ebf9e
slizevik <koreneva-03@list.ru>
2025-11-11 12:30:14 +0500

    lab_3

--- yooyyop.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.linear_model import LinearRegression
-
-X = np.array([-1, 1, 2, 3, 4]).reshape(-1, 1)
-y = [6.41, -0.79, -4.39, -7.99, -11.59]
-
-model = LinearRegression()
-model.fit(X, y)
-print(model.intercept_, model.coef_)
